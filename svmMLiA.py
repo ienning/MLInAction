8f5be83d48c99027f0285cc53c41685d504d79ca
ienning <enningwan@gmail.com>
2020-08-29 22:03:26 +0800

    no

---
+++ svmMLiA.py
@@ -0,0 +1,26 @@
+import random
+
+
+def loadDataSet(fileName):
+    dataMat = []
+    labelMat = []
+    # fr = open("E:\workfor2020\selfWork\ML\action\machinelearninginaction\Ch05\testSet.txt")
+    fr = open(fileName)
+    for line in fr.readlines():
+        lineArr = line.strip().split("\t")
+        dataMat.append([float(lineArr[0]), float(lineArr[1])])
+        labelMat.append(float(lineArr[2]))
+    return dataMat, labelMat
+
+def selectJrand(i, m):
+    j = i
+    while(j == i):
+        j = int(random.uniform(0, m))
+    return j
+
+def clipAlpha(aj, H, L):
+    if aj > H:
+        aj = H
+    if L > aj:
+        aj = L
+    return aj
--- treePlotlib.py
+++ treePlotlib.py
@@ -10,17 +10,24 @@ arrow_args = dict(arrowstyle="<-")
 def plotNode( nodeTxt, centerPt, parentPt, nodeType):
     createPlot.ax1.annotate( nodeTxt, xy=parentPt, xycoords="axes fraction", xytext=centerPt, textcoords="axes fraction", va="center", ha="center", bbox=nodeType, arrowprops=arrow_args)
 
-def createPlot():
+def createPlot(inTree):
     fig = plt.figure(1, facecolor="white")
     fig.clf()
-    createPlot.ax1 = plt.subplot(111, frameon=False)
-    plotNode( "决策节点", (0.5, 0.1), (0.1, 0.5), decisionNode )
-    plotNode( "叶节点", (0.8, 0.1), (0.3, 0.8), leafNode )
+    #createPlot.ax1 = plt.subplot(111, frameon=False)
+    #plotNode( "决策节点", (0.5, 0.1), (0.1, 0.5), decisionNode )
+    #plotNode( "叶节点", (0.8, 0.1), (0.3, 0.8), leafNode )
+    axprops = dict( xticks=[], yticks=[] )
+    createPlot.ax1 = plt.subplot(111, frameon=False, **axprops )
+    plotTree.totalW = float(getNumLeafs(inTree))
+    plotTree.totalD = float(getTreeDepth(inTree))
+    plotTree.xOff = -0.5/plotTree.totalW
+    plotTree.yOff = 1.0
+    plotTree(inTree, (0.5, 1.0), "")
     plt.show()
 
 def getNumLeafs(myTree):
     numLeafs = 0
-    firstStr = myTree.keys()[0]
+    firstStr = list(myTree.keys())[0]
     secondDict = myTree[firstStr]
     for key in secondDict.keys():
         if type(secondDict[key]).__name__ == "dict":
@@ -28,10 +35,9 @@ def getNumLeafs(myTree):
         else:
             numLeafs += 1
     return numLeafs
-
 def getTreeDepth(myTree):
     maxDepth = 0
-    firstStr = myTree.keys()[0]
+    firstStr = list(myTree.keys())[0]
     secondDict = myTree[firstStr]
     for key in secondDict.keys():
         if type(secondDict[key]).__name__ == "dict":
@@ -44,4 +50,22 @@ def getTreeDepth(myTree):
 def plotMidText( cntrPt, parentPt, txtString ):
     xMid = (parentPt[0] - cntrPt[0]) / 2.0 + cntrPt[0]
     yMid = (parentPt[1] - cntrPt[1]) / 2.0 + cntrPt[1]
-    createPlot().ax1.text( xMid, yMid, txtString )
+    createPlot.ax1.text( xMid, yMid, txtString )
+
+def plotTree( myTree, parentPt, nodeTxt ):
+    numLeafs = getNumLeafs(myTree)
+    depth = getTreeDepth(myTree)
+    firstStr = list(myTree.keys())[0]
+    cntrPt = (plotTree.xOff + (1.0 + float(numLeafs))/2.0/plotTree.totalW, plotTree.yOff)
+    plotMidText(cntrPt, parentPt, nodeTxt)
+    plotNode( firstStr, cntrPt, parentPt, decisionNode )
+    secondDict = myTree[firstStr]
+    plotTree.yOff = plotTree.yOff - 1.0/plotTree.totalD
+    for key in secondDict.keys():
+        if type(secondDict[key]).__name__ == "dict":
+            plotTree(secondDict[key], cntrPt, str(key) )
+        else:
+            plotTree.xOff = plotTree.xOff + 1.0/plotTree.totalW
+            plotNode( secondDict[key], (plotTree.xOff, plotTree.yOff ), cntrPt, leafNode )
+            plotMidText(( plotTree.xOff, plotTree.yOff, ), cntrPt, str(key) )
+    plotTree.yOff = plotTree.yOff + 1.0/plotTree.totalD
