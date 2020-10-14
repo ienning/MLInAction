9ab2c0f802141033ed1a4ec823728574fa1555f7
ienning <enningwan@gmail.com>
2020-10-14 20:31:33 +0800

    regression

--- regression.py
+++ regression.py
@@ -1,4 +1,8 @@
 from numpy import *
+from time import sleep
+import json
+import urllib3
+import urllib.request
 def loadDataSet(fileName):
     numFeat = len(open(fileName).readline().split("\t")) - 1
     dataMat = []
@@ -49,13 +53,14 @@ def lwlrTest(testArr, xArr, yArr, k = 1.0):
 def rssError(yArr, yHatArr):
     return ((yArr - yHatArr)**2).sum()
 
+# 岭回归算法
 def ridgeRegres(xMat, yMat, lam=0.2):
     xTx = xMat.T*xMat
     denom = xTx + eye(shape(xMat)[1])*lam
-    if linalg.get(denom) == 0.0:
+    if linalg.det(denom) == 0.0:
         print("This matrix is signular, cannot od inverse")
         return
-    ws = denom.I * (xMat.T*xMat)
+    ws = denom.I * (xMat.T*yMat)
     return ws
 
 def ridgeTest(xArr, yArr):
@@ -68,7 +73,78 @@ def ridgeTest(xArr, yArr):
     xMat = (xMat - xMeans)/xVar
     numTestPts = 30
     wMat = zeros((numTestPts, shape(xMat)[1]))
+    #print("wMat shape is ", shape(wMat))
     for i in range(numTestPts):
         ws = ridgeRegres(xMat, yMat, exp(i-10))
+        #print("wMat shape is ", shape(ws))
         wMat[i, :] = ws.T
     return wMat
+
+# 前向逐步回归
+def stageWise(xArr, yArr, eps=0.01, numIt = 100):
+    xMat = mat(xArr)
+    yMat = mat(yArr).T
+    # 求均值，且对在后面参数为0时，压缩行，对各列求均值，返回1*n，为1时是对压缩列
+    yMean = mean(yMat, 0)
+    yMat = yMat - yMean
+    xMat = regularize(xMat)
+    m,n = shape(xMat)
+    returnMat = zeros((numIt, n))
+    ws = zeros((n, 1))
+    wsTest = ws.copy()
+    wsMax = ws.copy()
+    for i in range(numIt):
+        print(ws.T)
+        lowestError = inf
+        for j in range(n):
+            for sign in [-1, 1]:
+                wsTest = ws.copy()
+                wsTest[j] += eps.sign
+                yTest = xMat*wsTest
+                rssE = rssError(yMat.A, yTest.A)
+                if rssE < lowestError:
+                    lowestError = rssE
+                    wsMax = wsTest
+        ws = wsMax.copy()
+        returnMat[i,:] = ws.T
+    return returnMat
+
+def regularize(xMat):
+    inMat = xMat.copy()
+    inMeans = np.mean(inMat, 0)
+    inVar = np.var(inMat, 0)
+    inMat = (inMat - inMeans) / inVar
+    return inMat
+
+# collect data
+def searchForSet(retX, retY, setNum, yr, numPce, origPrc):
+    sleep(10)
+    myAPIstr = "AIzaSyAxHylCf1PYtWjoN4tcu-nyh8-n9D0qoys"
+    searchURL = "https://www.googleapis.com/shopping/search/v1/public/products?key=%s&country=US&q=lego+%d&alt=json" % (myAPIstr, setNum)
+    pg = urllib.request.urlopen(searchURL)
+
+    retDict = json.loads(pg.read())
+    for i in range(len(retDict["item"])):
+        try:
+            currItem = retDict["item"][i]
+            if currItem["product"]["condition"] == "new":
+                newFlag = 1
+            else:
+                newFlag = 0
+            listOfInv = currItem["product"]["inventories"]
+            for item in listOfInv:
+                sellingPrice = item["price"]
+                if sellingPrice > origPrc * 0.5:
+                    print("%d\t%d\t%d\t%f\t%f" % (yr, numPce, newFlag, origPrc, sellingPrice))
+                    retX.append([yr, numPce, newFlag, origPrc])
+                    retY.append(sellingPrice)
+        except:
+            print("problem with %d"  % i)
+
+def setDataCollect(retX, retY):
+    searchForSet(retX, retY, 8288, 2006, 800, 49.99)
+    searchForSet(retX, retY, 10030, 2002, 3096, 269.99)
+    searchForSet(retX, retY, 10179, 2007, 5195, 499.99)
+    searchForSet(retX, retY, 10181, 2007, 3428, 199.99)
+    searchForSet(retX, retY, 10189, 2008, 5922, 299.99)
+    searchForSet(retX, retY, 10196, 2009, 3263, 249.99)
